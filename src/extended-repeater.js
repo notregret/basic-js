79a15ddd66e0c4f6261d09c10dac0733093a7f33
notregret <sanaxxx322@gmail.com>
2021-02-24 18:15:24 +0300

    fix: new solution

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,6 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function repeater(str, options) {
-  
-  // remove line with error and write your code here
+
 };
   
--- src/what-season.js
+++ src/what-season.js
@@ -1,6 +1,28 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function getSeason(/* date */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function getSeason(date) {
+    if (date === undefined){
+        return 'Unable to determine the time of year!';
+    }
+    if (Object.getPrototypeOf(date) !== Date.prototype || date.getMonth !== Date.prototype.getMonth) {
+        throw new Error('Not date');
+    }
+    switch (date.getMonth()+1){
+        case 1:
+        case 2:
+        case 12:
+            return 'winter';
+        case 3:
+        case 4:
+        case 5:
+            return 'spring';
+        case 6:
+        case 7:
+        case 8:
+            return 'summer';
+        case 9:
+        case 10:
+        case 11:
+            return 'autumn';
+    }
 };
